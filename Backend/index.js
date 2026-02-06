19958c569d869f9bee30bbd2bf71fcfb59cc77dd
RAVI <raviisharmaa.2004@gmail.com>
2026-02-06 11:10:39 +0530

    Configure CORS to allow localhost:5173hh

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+      origin: "http://localhost:5173",
+  credentials: true
+}
+));
 
 
 import route from "./routes/urlroutes.route.js";
